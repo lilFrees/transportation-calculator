721aa22dcca71d38bd0bd2724187d873208179a5
Bobomurod <111354555+lilFrees@users.noreply.github.com>
2023-07-27 13:51:13 +0500

    Add files via upload

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,14 @@
+import AlignCenter from "../../UI/AlignCenter/AlignCenter";
+import style from "./Footer.module.css";
+
+const Footer = function () {
+  return <div className={style.footer}>
+    <AlignCenter>
+      <div className={style.container}>
+        
+      </div>
+    </AlignCenter>
+  </div>;
+};
+
+export default Footer;
